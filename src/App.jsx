e259e91f851610d0f3d0bea75ae0c864ade4cfd5
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-06 01:21:00 +0530

    adjusting

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
         >
           <img src="./imagelensiportrait3.avif" alt="" />
         </div> */} 
-        <p className=" font-['Give_You_Glory'] fixed w-full text-center tracking-tight text-9xl  text-emerald-200  top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] ">
+        <p className=" font-['Give_You_Glory'] fixed w-full text-center tracking-tight text-9xl font-semibold bg-gradient-to-br p-9 from-emerald-400 to-emerald-700-400 bg-clip-text text-transparent   top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] ">
           Darkroom Engineering{" "}
         </p>
         <div
@@ -77,13 +77,13 @@ function App() {
           className="elem col-span-1 row-span-1"
           style={{ "--r": 2, "--c": 2 }}
         >
-          <img src="./imagelensiportrait7.avif" alt="" />
+          <img src="./imagelenisportrait5.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 2, "--c": 6 }}
         >
-          <img src="./imagelensiportrait8.avif" alt="" />
+          <img src="./imagelenisportrait4.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
@@ -101,13 +101,13 @@ function App() {
           className="elem col-span-1 row-span-1"
           style={{ "--r": 4, "--c": 1 }}
         >
-          <img src="./imagelensiportrait7.avif" alt="" />
+          <img src="./imagelensiportrait6.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 4, "--c": 4 }}
+          style={{ "--r": 4, "--c": 6 }}
         >
-          <img src="./imagelensiportrait8.avif" alt="" />
+          <img src="./imagelensiportrait9.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
@@ -117,15 +117,15 @@ function App() {
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 5, "--c": 6 }}
+          style={{ "--r": 5, "--c": 7}}
         >
-          <img src="./imagelensiportrait8.avif" alt="" />
+          <img src="./imagelensiportrait4.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 6, "--c": 3 }}
         >
-          <img src="./imagelensiportrait7.avif" alt="" />
+          <img src="./imagelensiportrait3.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
@@ -159,10 +159,34 @@ function App() {
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 1, "--c": 3 }}
+          style={{ "--r": 8, "--c": 3 }}
         >
           <img src="./imagelensiportrait7.avif" alt="" />
         </div>
+        <div
+          className="elem col-span-1 row-span-1"
+          style={{ "--r": 8, "--c": 5 }}
+        >
+          <img src="./imagelensiportrait8.avif" alt="" />
+        </div>
+        <div
+          className="elem col-span-1 row-span-1"
+          style={{ "--r": 8, "--c": 3 }}
+        >
+          <img src="./imagelensiportrait7.avif" alt="" />
+        </div>
+        {/* <div
+          className="elem col-span-1 row-span-1"
+          style={{ "--r": 8, "--c": 6 }}
+        >
+          <img src="./imagelensiportrait8.avif" alt="" />
+        </div> */}
+        {/* <div
+          className="elem col-span-1 row-span-1"
+          style={{ "--r": 1, "--c": 3 }}
+        >
+          <img src="./imagelensiportrait7.avif" alt="" />
+        </div> */}
         {/* <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 1, "--c": 3 }}
