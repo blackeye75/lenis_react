9f0dc3a294d2e8cb814102a6ced4ca7e37bb61b0
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-08 19:56:47 +0530

    placement done

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <div className="w-full  bg-zinc-900">
-      <div className="main grid grid-cols-8 p-8 grid-rows-20 gap-2 overflow-hidden">
+      <div className="main grid grid-cols-8 p-8 grid-rows-20 gap-2 overflow-hidden space-y-6">
         {/* <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 1, "--c": 3 }}
@@ -63,118 +63,118 @@ function App() {
         </p>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 1, "--c": 3 }}
+          style={{ "--r": 1, "--c": 2 }}
         >
-          <img src="./imagelensiportrait7.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait7.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 1, "--c": 7 }}
         >
-          <img src="./imagelensiportrait8.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait8.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 2, "--c": 2 }}
+          style={{ "--r": 2, "--c": 5 }}
         >
-          <img src="./imagelenisportrait5.avif" alt="" />
+          <img className="rounded-lg" src="./imagelenisportrait5.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 2, "--c": 6 }}
+          style={{ "--r": 2, "--c": 1 }}
         >
-          <img src="./imagelenisportrait4.avif" alt="" />
+          <img className="rounded-lg" src="./imagelenisportrait4.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 3, "--c": 4 }}
         >
-          <img src="./imagelensiportrait7.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait7.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 3, "--c": 8 }}
         >
-          <img src="./imagelensiportrait8.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait8.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 4, "--c": 1 }}
         >
-          <img src="./imagelensiportrait6.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait6.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 4, "--c": 6 }}
         >
-          <img src="./imagelensiportrait9.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait9.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 5, "--c": 2 }}
+          style={{ "--r": 5, "--c": 3 }}
         >
-          <img src="./imagelensiportrait7.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait7.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 5, "--c": 7}}
+          style={{ "--r": 5, "--c": 8}}
         >
-          <img src="./imagelensiportrait4.avif" alt="" />
+          <img className="rounded-lg" src="./imagelenisportrait4.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 6, "--c": 3 }}
+          style={{ "--r": 6, "--c": 1 }}
         >
-          <img src="./imagelensiportrait3.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait3.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 6, "--c": 3 }}
+          style={{ "--r": 6, "--c": 7 }}
         >
-          <img src="./imagelensiportrait8.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait8.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 7, "--c": 3 }}
         >
-          <img src="./imagelensiportrait7.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait7.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 7, "--c": 8 }}
         >
-          <img src="./imagelensiportrait8.avif" alt="" />
+          <img className="rounded-lg" src="./imagelensiportrait8.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 8, "--c": 3 }}
+          style={{ "--r": 8, "--c": 4 }}
+        >
+          <img className="rounded-lg" src="./imagelensiportrait7.avif" alt="" />
+        </div>
+        <div
+          className="elem col-span-1 row-span-1"
+          style={{ "--r": 8, "--c": 7 }}
+        >
+          <img className="rounded-lg" src="./imagelensiportrait8.avif" alt="" />
+        </div>
+        {/* <div
+          className="elem col-span-1 row-span-1"
+          style={{ "--r": 0, "--c": 3 }}
         >
           <img src="./imagelensiportrait7.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 8, "--c": 5 }}
+          style={{ "--r": 0, "--c": 5 }}
         >
           <img src="./imagelensiportrait8.avif" alt="" />
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 8, "--c": 3 }}
+          style={{ "--r": 0, "--c": 4 }}
         >
           <img src="./imagelensiportrait7.avif" alt="" />
-        </div>
-        <div
-          className="elem col-span-1 row-span-1"
-          style={{ "--r": 8, "--c": 5 }}
-        >
-          <img src="./imagelensiportrait8.avif" alt="" />
-        </div>
-        <div
-          className="elem col-span-1 row-span-1"
-          style={{ "--r": 8, "--c": 3 }}
-        >
-          <img src="./imagelensiportrait7.avif" alt="" />
-        </div>
+        </div> */}
         {/* <div
           className="elem col-span-1 row-span-1"
           style={{ "--r": 8, "--c": 6 }}
