f6b8682e9050c196a198dd001ac90fe2d1540e45
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-09 16:44:42 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import Lenis from "lenis";
 import "./App.css";
 import { useEffect } from "react";
 import "./index.css";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger);
   useEffect(() => {
     const lenis = new Lenis();
 
     lenis.on("scroll", (e) => {
-      console.log(e);
+      // console.log(e);
     });
 
     function raf(time) {
@@ -17,6 +21,38 @@ function App() {
     }
 
     requestAnimationFrame(raf);
+  });
+
+  useGSAP(() => {
+    document.querySelectorAll(".elem").forEach((elem) => {
+      let image = elem.querySelector("img");
+      let tl = gsap.timeline();
+      let xTransform = gsap.utils.random(-100, 100);
+      tl.set(
+        image,
+        { transformOrigin: `${xTransform < 0 ? 0 : "100%"}` },
+        "start"
+      )
+        .to(
+          image,
+          {
+            scale: 0,
+            ease: "none",
+            scrollTrigger: {
+              trigger: image,
+              start: "top top",
+              end: "bottom top",
+              scrub: true,
+            },
+          },
+          "start"
+        )
+        .to(elem, {
+          xPercent: xTransform,
+          ease: "none",
+          scrollTrigger: { trigger: image, start: "top bottom", end:"bottom top",scrub: true },
+        });
+    });
   });
 
   return (
@@ -57,8 +93,8 @@ function App() {
           style={{ "--r": 1, "--c": 3 }}
         >
           <img src="./imagelensiportrait3.avif" alt="" />
-        </div> */} 
-        <p className=" font-['Give_You_Glory'] fixed w-full text-center tracking-tight text-9xl font-semibold bg-gradient-to-br p-9 from-emerald-400 to-emerald-700-400 bg-clip-text text-transparent   top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] ">
+        </div> */}
+        <p className="z-10 font-['Give_You_Glory'] fixed w-full text-center tracking-tight text-9xl font-semibold bg-gradient-to-br p-9 from-blue-400 to-emerald-700-400 bg-clip-text text-transparent   top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] ">
           Darkroom Engineering{" "}
         </p>
         <div
@@ -117,7 +153,7 @@ function App() {
         </div>
         <div
           className="elem col-span-1 row-span-1"
-          style={{ "--r": 5, "--c": 8}}
+          style={{ "--r": 5, "--c": 8 }}
         >
           <img className="rounded-lg" src="./imagelenisportrait4.avif" alt="" />
         </div>
